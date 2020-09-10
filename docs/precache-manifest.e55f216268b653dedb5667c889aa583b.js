109b81ea419909c01331edebde5601fe71cfe76d
Steven Montero <steven.monteromunoz@ucrso.info>
2020-09-10 00:08:20 -0600

    docs agregado

---
+++ docs/precache-manifest.e55f216268b653dedb5667c889aa583b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06481c3c1dcdaa114464bd427aab18df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9ad9fb9210ee4de0798",
+    "url": "/static/css/main.a92caae2.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ad9fb9210ee4de0798",
+    "url": "/static/js/main.bfc5fcef.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
